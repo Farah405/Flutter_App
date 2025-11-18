72540f33c8f9701249987d6a053ba4d1f645cd42
Kero <Kerlosromany7@gmail.com>
2025-11-19 00:44:59 +0200

    Changing login color to pink

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class _MyAppState extends State<MyApp> {
                   style: TextStyle(
                     fontSize: 36,
                     fontWeight: FontWeight.bold,
-                    color: Colors.blue,
+                    color: Colors.pink,
                   ),
                 ),
                 SizedBox(height: 8),
